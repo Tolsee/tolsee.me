9062986feda538e83b1fd3978fa4c86f449cff01
Tulsi Sapkota <tolsee3@gmail.com>
2023-01-29 19:32:51 +0545

    Update webpack config

--- next.config.js
+++ next.config.js
@@ -3,26 +3,6 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-        //     config.module.rules.push({
-        //         test: /\.node$/,
-        //         loader: "node-loader",
-        //     });
-        //
-        //     config.externals["canvas"] = {};
-        //     console.log(config.externals);
-            config.externals.push("canvas");
-            config.externals.push("utf-8-validate");
-            config.externals.push("butterutil");
-        //         canvas: "commonjs canvas",
-        //         "utf-8-validate": "utf-8-validate",
-        //         bufferutil: "bufferutil",
-        //     });
-        }
-
-        return config;
-    },
 };
 
 module.exports = nextConfig;
