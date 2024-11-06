e7a2f55cdbb8f2df29cd25758863adaf1f5a3633
Tulsi Sapkota <tolsee3@gmail.com>
2024-11-06 22:07:27 +1100

    feat: meta data

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,11 +1,13 @@
 import { getAllPosts, getPostBySlug } from '@/lib/api';
 import markdownToHtml from '@/lib/markdownToHtml';
 import { PostTitle } from '@/src/components/post/post-title';
+import { notFound } from "next/navigation";
 
 import markdownStyles from './markdown-styles.module.css';
 import './prism-theme.css';
 import './remark.css';
-import 'remark-github-blockquote-alert/alert.css'
+import 'remark-github-blockquote-alert/alert.css';
+import { Metadata } from 'next';
 
 // TODO: Change later
 // import './remark-alert.css';
@@ -18,11 +20,32 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function Post({
-  params: { slug },
-}: {
-  params: { slug: string };
-}) {
+type Params = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export async function generateMetadata(props: Params): Promise<Metadata> {
+  const { slug } = await props.params;
+  const post = await getPostBySlug(slug, ['title', 'author', 'content']);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const title = `${post.title} | Tulsi Sapkota`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+    },
+  };
+}
+
+export default async function Post({ params }: Params) {
+  const { slug } = await params;
   const post = await getPostBySlug(slug, ['title', 'author', 'content']);
   const content = await markdownToHtml(post.content || '');
 
